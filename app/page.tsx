dd40e60e26f022627e2a431dba94055268541ad1
mafaro21 <mafaro2105@gmail.com>
2025-02-06 16:54:55 +0200

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,82 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <nav className="p-3 px-6">
+        {/* search form  */}
+        <div className="flex">
+          <form className="flex w-full">
+            <input placeholder="Search" className="rounded-md w-5/6 p-1" />
+            <button className="bg-blue-600 ml-2 w-24 p-1 rounded-md text-white">Search</button>
+          </form>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <button className="border-2 border-blue-300 w-96 text-left p-1 rounded-md ">Company Name Here</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </nav>
+
+      <div className="w-full mt-5">
+        <div className="mx-10 px-10 ">
+          <p className="text-4xl text-blue-600">Assessment</p>
+          <div className="text-sm mt-4">
+            <p className="italic">blafdlkafhdlkhaflkdhsaflkdhsal khfdlksahf dlsahf lkdshaf lkdhafldhsflkdhaklfhdklsahfdklshaflkdhsaklfhdslka hlkdsah kldhsa flkdhsa lk</p>
+            <p className="italic">blafdlkafhdlkhaflkdhsaflkdhsal lafjdkla jlkd jafld jakfldjak khfdlksahf dlsahf lkdshaf lkdhafldhsflkdhaklfhdklsahfdklshaflkdhsaklfhdslka hlkdsah kldhsa flkdhsa lk</p>
+            <p className="italic">blafdlkafhdlkhaflkdhsaflkdhsal khfdlksahf  dlajfk ldja lkfjd lkajf dklsajfkldlsahf lkdshaf lkdhafldhsflkdhaklfhdklsahfdklshaflkdhsaklfhdslka hlkdsah kldhsa flkdhsa lk</p>
+          </div>
+
+          <div>
+            <p className="text-2xl mt-8 text-blue-600">Question Categories</p>
+
+            <table className="min-w-full border-collapse border-gray-300 text-sm" >
+              <thead>
+                <tr className="">
+                  <th className="px-4 py-2 text-left">Domain</th>
+                  <th className="px-4 py-2 text-left">Sub-Domain</th>
+                  <th className="px-4 py-2 text-left">Description</th>
+                  <th className="px-4 py-2 text-left">Questionnaire</th>
+                  <th className="px-4 py-2 text-left">Questions</th>
+                  <th className="px-4 py-2 text-left">Remediations</th>
+                  <th className="px-4 py-2 text-left">Status</th>
+                  <th className="px-4 py-2 text-left">Compliance</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="px-4 py-2">Policy and Process</td>
+                  <td className="px-4 py-2">Information Security Policy</td>
+                  <td className="px-4 py-2">Third Party Information Security Assessment (TIA)</td>
+                  <td className="px-4 py-2"><button className="bg-blue-600 p-1 px-3 rounded-md text-white "><b>Open</b></button></td>
+                  <td className="px-4 py-2">8/8</td>
+                  <td className="px-4 py-2">0/0</td>
+                  <td className="px-4 py-2">Completed</td>
+                  <td className="px-4 py-2">Compliant (100%)</td>
+
+                </tr>
+                <tr className="">
+                  <td className="px-4 py-2">Security Organization</td>
+                  <td className="px-4 py-2">External Parties, Internal Organization, Risk Management</td>
+                  <td className="px-4 py-2">Third Party Information Security Assessment (TIA)</td>
+                  <td className="px-4 py-2"><button className="bg-blue-600 p-1 px-3 rounded-md text-white "><b>Open</b></button></td>
+                  <td className="px-4 py-2">11/11</td>
+                  <td className="px-4 py-2">0/0</td>
+                  <td className="px-4 py-2">Completed</td>
+                  <td className="px-4 py-2">Not Compliant (78%)</td>
+                </tr>
+                <tr>
+                  <td className="px-4 py-2">Human Resource Security</td>
+                  <td className="px-4 py-2">Onboarding, Prior to Employment, Termination & Change of employment</td>
+                  <td className="px-4 py-2">Third Party Information Security Assessment (TIA)</td>
+                  <td className="px-4 py-2"><button className="bg-blue-600 p-1 px-3 rounded-md text-white "><b>Open</b></button></td>
+                  <td className="px-4 py-2">6/6</td>
+                  <td className="px-4 py-2">0/0</td>
+                  <td className="px-4 py-2">Completed</td>
+                  <td className="px-4 py-2">Not Compliant (70%)</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
