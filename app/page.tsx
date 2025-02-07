8a21d47df0de59dd962a7be3b222cf118a1cac2b
mafaro21 <mafaro2105@gmail.com>
2025-02-07 12:17:07 +0200

    perfecting structure

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,70 @@ export default function Home() {
 
       <div className="w-full mt-5">
         <div className="mx-10 px-10 ">
-          <p className="text-4xl text-blue-600">Assessment</p>
+          <p className="text-4xl text-blue-600">Third Party Assessment</p>
           <div className="text-sm mt-4">
-            <p className="italic">blafdlkafhdlkhaflkdhsaflkdhsal khfdlksahf dlsahf lkdshaf lkdhafldhsflkdhaklfhdklsahfdklshaflkdhsaklfhdslka hlkdsah kldhsa flkdhsa lk</p>
-            <p className="italic">blafdlkafhdlkhaflkdhsaflkdhsal lafjdkla jlkd jafld jakfldjak khfdlksahf dlsahf lkdshaf lkdhafldhsflkdhaklfhdklsahfdklshaflkdhsaklfhdslka hlkdsah kldhsa flkdhsa lk</p>
-            <p className="italic">blafdlkafhdlkhaflkdhsaflkdhsal khfdlksahf  dlajfk ldja lkfjd lkajf dklsajfkldlsahf lkdshaf lkdhafldhsflkdhaklfhdklsahfdklshaflkdhsaklfhdslka hlkdsah kldhsa flkdhsa lk</p>
+            <p className="italic text-gray-500">This third party assessment on Third Party Information Security Assessment (TIA) has been sent to you by Unilever, due for the 14 June 2024.</p>
+            <p className="italic mt-4 text-gray-500">To begin, select a category and select the [Start] button to generate the corresponding questionnaire. Click the document link that appears to navigate to and answer the questionnaire.</p>
+            <p className="italic mt-4 text-gray-500">All the assigned question categories must be completed before the [Submit] button becomes available to complete the assessment and send it back to Unilever for review and approval</p>
+          </div>
+
+          <div>
+            <p className="text-2xl mt-8 text-blue-600">Summary</p>
+
+            <table className="min-w-full border-collapse border-gray-300 text-sm ml-9" >
+              <tbody>
+                <tr className="">
+                  <td className="px-4 py-2 text-left italic">Assessment Types:</td>
+                  <td className="px-4 py-2 text-left">Third Party Information Security Assessment (TIA)</td>
+                  <td className="px-4 py-2 text-left"></td>
+                  <td className="px-4 py-2 text-left"></td>
+                  <td className="px-4 py-2 text-left"></td>
+                </tr>
+                <tr>
+                  <td className="px-4 py-2 italic">Sent By:</td>
+                  <td className="px-4 py-2">Unilever</td>
+                  <td className="px-4 py-2 italic">Reviewer(s):</td>
+                  <td className="px-4 py-2">Kisalaya Kurariya, Nithin Venkatesh</td>
+                  <td className="px-4 py-2 italic">Date Sent:</td>
+                  <td className="px-4 py-2">7 Jun 2024</td>
+
+                </tr>
+                <tr className="">
+                  <td className="px-4 py-2 italic">To:</td>
+                  <td className="px-4 py-2">Rafferty Investments TA Providence</td>
+                  <td className="px-4 py-2 italic">Recipient(s):</td>
+                  <td className="px-4 py-2">Farai, lainah, Tanyaradwza, Tineyi Zimbudzana, Tinotenda Zinyeka</td>
+                  <td className="px-4 py-2 italic">Date Due:</td>
+                  <td className="px-4 py-2">14 Jun 2024</td>
+                </tr>
+
+                {/* <tr className="">
+                  <td className="px-4 py-2 text-left italic">Risk Rating Score:</td>
+                  <td className="px-4 py-2 text-left"></td>
+                  <td className="px-4 py-2 text-left"></td>
+                  <td className="px-4 py-2 text-left"></td>
+                  <td className="px-4 py-2 text-left italic">Assessment Compliance:</td>
+                  <td className="px-4 py-2 text-left">Compliant</td>
+                  <td className="px-4 py-2 text-left italic">Compliance Value:</td>
+                  <td className="px-4 py-2 text-left">92%</td>
+                </tr> */}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="flex justify-between ml-16 mt-16 text-sm">
+            <div className="italic">Risk Rating Score:</div>
+            <div></div>
+
+            <div className="flex ">
+              <div className="italic">Assessment Compliance:</div>
+              <div className="ml-5">Compliant</div>
+            </div>
+
+            <div className="flex ">
+              <div className="italic">Compliance Value:</div>
+              <div className="ml-5">92%</div>
+            </div>
           </div>
 
           <div>
@@ -45,7 +104,7 @@ export default function Home() {
                   <td className="px-4 py-2">Policy and Process</td>
                   <td className="px-4 py-2">Information Security Policy</td>
                   <td className="px-4 py-2">Third Party Information Security Assessment (TIA)</td>
-                  <td className="px-4 py-2"><button className="bg-blue-600 p-1 px-3 rounded-md text-white "><b>Open</b></button></td>
+                  <td className="px-4 py-2 hover:bg-blue-100"><button className="bg-blue-600 hover:bg-blue-800 p-1 px-3 rounded-md text-white "><b>Open</b></button></td>
                   <td className="px-4 py-2">8/8</td>
                   <td className="px-4 py-2">0/0</td>
                   <td className="px-4 py-2">Completed</td>
@@ -56,7 +115,7 @@ export default function Home() {
                   <td className="px-4 py-2">Security Organization</td>
                   <td className="px-4 py-2">External Parties, Internal Organization, Risk Management</td>
                   <td className="px-4 py-2">Third Party Information Security Assessment (TIA)</td>
-                  <td className="px-4 py-2"><button className="bg-blue-600 p-1 px-3 rounded-md text-white "><b>Open</b></button></td>
+                  <td className="px-4 py-2 hover:bg-blue-100"><button className="bg-blue-600 hover:bg-blue-800 p-1 px-3 rounded-md text-white "><b>Open</b></button></td>
                   <td className="px-4 py-2">11/11</td>
                   <td className="px-4 py-2">0/0</td>
                   <td className="px-4 py-2">Completed</td>
@@ -66,14 +125,19 @@ export default function Home() {
                   <td className="px-4 py-2">Human Resource Security</td>
                   <td className="px-4 py-2">Onboarding, Prior to Employment, Termination & Change of employment</td>
                   <td className="px-4 py-2">Third Party Information Security Assessment (TIA)</td>
-                  <td className="px-4 py-2"><button className="bg-blue-600 p-1 px-3 rounded-md text-white "><b>Open</b></button></td>
+                  <td className="px-4 py-2 hover:bg-blue-100"><button className="bg-blue-600 hover:bg-blue-800 p-1 px-3 rounded-md text-white "><b>Open</b></button></td>
                   <td className="px-4 py-2">6/6</td>
                   <td className="px-4 py-2">0/0</td>
                   <td className="px-4 py-2">Completed</td>
                   <td className="px-4 py-2">Not Compliant (70%)</td>
                 </tr>
+
               </tbody>
+
             </table>
+            <div className='flex justify-end mt-6'>
+              <button className="bg-blue-600 p-1 px-3 rounded-md text-white "><b>Submit</b></button>
+            </div>
           </div>
         </div>
       </div>
